43886bf7a447496446209f6c6686f50cdd05e200
denial-E <shempeter221992@gmail.com>
2025-07-02 02:05:38 +0530

    run

--- Routers/employee.router.js
+++ Routers/employee.router.js
@@ -2,14 +2,14 @@ import express from "express";
 import {
   createEmployee,
   getEmployeeDetailById,
-  getEmployeedetail,
+  getEmployeeDetail,
   updateEmployeeById,
 } from "../Controllers/employee.controller.js";
 
 const router = express.Router();
 
 router.post("/create-emp", createEmployee); //http://localhost:4000/api/create-emp
-router.get("/get-empdetails", getEmployeedetail); //http://localhost:4000/api/get-empdetails
+router.get("/get-empdetails", getEmployeeDetail); //http://localhost:4000/api/get-empdetails
 router.get("/getempdetails/:id", getEmployeeDetailById); //http://localhost:4000/api/getempdetails/:id
 router.put("/edit-emp/:id", updateEmployeeById);//http://localhost:4000/api/edit-emp/:id
 export default router;
