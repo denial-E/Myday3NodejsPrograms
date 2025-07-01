076bed38518c9c2d7bb4ab196c53cc413693a1c4
denial-E <shempeter221992@gmail.com>
2025-07-02 02:36:41 +0530

    run

--- Routers/employee.router.js
+++ Routers/employee.router.js
@@ -8,8 +8,8 @@ import {
 
 const router = express.Router();
 
-router.post("/create-emp", createEmployee); //http://localhost:4000/api/create-emp
-router.get("/get-empdetails", getEmployeeDetail); //http://localhost:4000/api/get-empdetails
-router.get("/getempdetails/:id", getEmployeeDetailById); //http://localhost:4000/api/getempdetails/:id
-router.put("/edit-emp/:id", updateEmployeeById);//http://localhost:4000/api/edit-emp/:id
+router.post("/create-emp", createEmployee); //http://localhost:4000/peter/create-emp
+router.get("/get-empdetails", getEmployeeDetail); //http://localhost:4000/peter/get-empdetails
+router.get("/getempdetails/:id", getEmployeeDetailById); //http://localhost:4000/peter/getempdetails/:id
+router.put("/edit-emp/:id", updateEmployeeById);//http://localhost:4000/peter/edit-emp/:id
 export default router;
